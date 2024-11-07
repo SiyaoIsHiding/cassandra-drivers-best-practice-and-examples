bc0bd51e61dc4f7728a8c372f7a5952b8a35a23a
janehe <siyaoh4@uci.edu>
2024-11-06 23:04:09 -0800

    query log

--- go/metrics-and-logging/basic-metrics-and-logging/main.go
+++ go/metrics-and-logging/basic-metrics-and-logging/main.go
@@ -7,11 +7,18 @@ import (
 	"os"
 )
 
+type myQueryObserver struct {
+	logger log.Logger
+}
+
+func (o *myQueryObserver) ObserveQuery(ctx context.Context, q gocql.ObservedQuery) {
+	o.logger.Printf("Query: %s, Latency: %v, Host: %s\n", q.Statement, q.Metrics.TotalLatency, q.Host.HostnameAndPort())
+}
+
 func main() {
 	cluster := gocql.NewCluster("127.0.0.1:9043", "127.0.0.1:9044", "127.0.0.1:9045")
 	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.DCAwareRoundRobinPolicy("datacenter1"))
-	cluster.Logger = log.New(os.Stdout, "gocql: ", log.LstdFlags)
-
+	//cluster.Logger = log.New(os.Stdout, "gocql: ", log.LstdFlags)
 	session, err := cluster.CreateSession()
 	if err != nil {
 		log.Fatalf("Error creating session: %v\n", err)
@@ -19,17 +26,19 @@ func main() {
 	}
 	defer session.Close()
 
+	queryObserver := &myQueryObserver{logger: *log.New(os.Stdout, "QueryObserver: ", log.LstdFlags)}
+
 	ctx := context.Background()
 
-	var host_id string
-	var broadcast_address string
-	scanner := session.Query("SELECT host_id, broadcast_address FROM system.local").WithContext(ctx).Iter().Scanner()
-	for scanner.Next() {
-		err := scanner.Scan(&host_id, &broadcast_address)
-		if err != nil {
-			log.Fatalf("Error scanning: %v\n", err)
-			return
-		}
-		log.Printf("host_id: %s, broadcast_address: %s\n", host_id, broadcast_address)
-	}
+	//var host_id string
+	//var broadcast_address string
+	session.Query("SELECT host_id, broadcast_address FROM system.local").WithContext(ctx).Observer(queryObserver).Iter().Scanner()
+	//for scanner.Next() {
+	//	err := scanner.Scan(&host_id, &broadcast_address)
+	//	if err != nil {
+	//		log.Fatalf("Error scanning: %v\n", err)
+	//		return
+	//	}
+	//	log.Printf("host_id: %s, broadcast_address: %s\n", host_id, broadcast_address)
+	//}
 }
